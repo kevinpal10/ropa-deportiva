24e54db6dbb3aaa1a37d6b63dad8f1cd80630ba3
Kevin Pallo <kevin-akd1@hotmail.com>
2026-04-16 15:02:51 -0500

    nuevo

--- src/app/services/producto/producto.service.ts
+++ src/app/services/producto/producto.service.ts
@@ -20,7 +20,7 @@ export interface Producto {
 
 
 export class ProductoService {
-  private apiUrl = 'http://18.218.140.13/api/productos/';
+  private apiUrl = 'https://18.218.140.13/api/productos/';
 
   constructor(private http: HttpClient) {}
 
