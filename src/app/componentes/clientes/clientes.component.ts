29a35cc06a8cb4c69bf67033609dd2c425a6d545
Kevin Pallo <kevin-akd1@hotmail.com>
2026-06-26 22:52:50 -0500

    Corrección camisetas

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -9,21 +9,9 @@ export class ClientesComponent {
 
   comments = [
     {
-      "nombre": "María S.",
-      "img": "./assets/imagenes/mujeres/img-b-16.jpg",
-      "comment": "Excelente calidad, el hoodie llegó rapidísimo y la talla fue perfecta. ¡Ya pedí otro!",
-      "rating": 5
-    },
-    {
-      "nombre": "Valeria R.",
-      "img": "./assets/imagenes/mujeres/img15.jpg",
-      "comment": "Me siento cómoda, hecho a mi medida. Los precios son muy accesibles.",
-      "rating": 5
-    },
-    {
-      "nombre": "Andrés P.",
-      "img": "./assets/imagenes/hombres/img7.jpg",
-      "comment": "Excelente servicio, coordinamos todo por WhatsApp. 5 estrellas.",
+      "nombre": "Josué A.",
+      "img": "./assets/imagenes/hombres/camiseta1.jpg",
+      "comment": "Camiseta elegante de buena calidad y muy comoda.",
       "rating": 5
     }
   ]
